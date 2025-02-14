a019aeadc58047e2c606d1840a5f852057a0e52c
SaiHtetAungKo <saihtetaungko24@gmail.com>
2025-02-14 16:05:56 +0630

    initial commit - php bootstrap project

---
+++ includes/footer.php
@@ -0,0 +1,7 @@
+<footer class="text-center mt-5 py-3 bg-light">
+        <p>&copy; <?php echo date("Y"); ?> My PHP Project</p>
+    </footer>
+    <script src="https://code.jquery.com/jquery-3.6.0.min.js"></script>
+    <script src="https://cdn.jsdelivr.net/npm/bootstrap@5.3.0/dist/js/bootstrap.bundle.min.js"></script>
+</body>
+</html>
---
+++ index.php
@@ -0,0 +1,6 @@
+<?php include 'includes/header.php'; ?>
+<div class="container">
+    <h1 class="text-center mt-5">Welcome to My PHP & Bootstrap Project</h1>
+    <p class="text-center">This is a simple PHP project with Bootstrap and jQuery.</p>
+</div>
+<?php include 'includes/footer.php'; ?>
