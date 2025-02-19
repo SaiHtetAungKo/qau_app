f330b7769056302092f514e72a1a967e12375da7
SaiHtetAungKo <saihtetaungko24@gmail.com>
2025-02-19 22:02:26 +0630

    fix: fix 1st task

--- index.php
+++ index.php
@@ -1,7 +1,7 @@
 <?php include 'includes/header.php'; ?>
 <div class="container">
     <h1 class="text-center mt-5">Welcome to My PHP & Bootstrap Project</h1>
-    <p class="text-center">This is a simple PHP project with Bootstrap and jQuery.</p>
+    <p class="text-center">This is a simple PHP project with Bootstrap and jQuery. testing</p>
 </div>
 <?php include 'includes/footer.php'; ?>
 
