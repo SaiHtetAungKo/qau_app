57fd7b3103b14edc2a7d5966fa86d987674e781b
SaiHtetAungKo <saihtetaungko24@gmail.com>
2025-02-22 21:59:22 +0630

    feat: implement codes for backend

--- index.php
+++ index.php
@@ -2,6 +2,7 @@
 <div class="container">
     <h1 class="text-center mt-5">Welcome to My PHP & Bootstrap Project</h1>
     <p class="text-center">This is a simple PHP project with Bootstrap and jQuery. testing</p>
+    <h1 class="text-center mt-5">Backend</h1>
 </div>
 <?php include 'includes/footer.php'; ?>
 
